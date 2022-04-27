35ea0a89149065351c6555cbca5e1ab8ba382a22
シンプルシェイプ <iamryoman@gmail.com>
2022-04-27 16:26:51 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -20,7 +23,9 @@ class Item < ApplicationRecord
   # 選択が「--」の時は保存できないようにする
   validates :category_id, :condition_id, :days_to_ship_id, :hipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: "指定の範囲で入力してください"}
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: "指定の範囲で入力してください"},
+  format: { with: /\A[0-9]+\z/ , message: "半角数字で入力してください"}
+
   def new
     # @prototype = Prototype.new
   end
