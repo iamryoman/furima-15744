4d26eb844f821c3029f58eac0d0f7135154b5804
シンプルシェイプ <iamryoman@gmail.com>
2022-06-09 21:31:50 +0900

    購入商品表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:edit, :destroy]
+
   def index
-    @order = Order.create(order_params)
+    @item = Item.new
+    @item = Item.find_by(params[:id])
   end
 
   def new
@@ -9,8 +13,26 @@ class OrdersController < ApplicationController
 
   def create
     @orders = Orders.create(orders_params)
+    @order = Order.create(order_params)
     Address.create(address_params)
     redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :postal_code,
+      :prefecture_id,
+      :municipalities,
+      :address,
+      :building_name,
+      :telephone_number
+    ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
 end
